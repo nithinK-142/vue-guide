019038d5e4e4576dd03ab6037efa5382f531d08a
nithinK-142 <nithin.sagar.359@gmail.com>
2024-01-22 13:05:30 +0530

    feat: basic guide for options or configuration options.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,53 @@
 <template>
-  <main>
-    <h1>Vue Demo App</h1>
-  </main>
+  <div>
+    <h1>{{ greeting }}</h1>
+    <p>Counter: {{ counter }}</p>
+    <p>Reversed Greeting: {{ reversedGreeting }}</p>
+    <button @click="incrementCounter">Increment Counter</button>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'John',
+      counter: 0,
+      reversedGreeting: '',
+    };
+  },
+  computed: {
+    greeting() {
+      return `Hello, ${this.name}!`;
+    },
+  },
+  methods: {
+    incrementCounter() {
+      this.counter++;
+    },
+    reverseName() {
+      this.reversedGreeting = this.name.split('').reverse().join('');
+    },
+  },
+  created() {
+    console.log('Component created');
+    this.reverseName();
+  },
+  mounted() {
+    console.log('Component mounted');
+  },
+  destroyed() {
+    console.log('Component destroyed');
+  },
+  watch: {
+    counter(newVal, oldVal) {
+      console.log(`Counter changed from ${oldVal} to ${newVal}`);
+      this.reverseName();
+    },
+    name(newVal, oldVal) {
+      console.log(`Name changed from ${oldVal} to ${newVal}`);
+      this.reverseName();
+    },
+  },
+};
+</script>
