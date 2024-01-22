737bc6d261ea1f2e13c448ff42d7430b05b6ba0d
nithinK-142 <nithin.sagar.359@gmail.com>
2024-01-22 13:01:13 +0530

    feat: pinia getting started.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,35 @@
+<script setup>
+import { useCounterStore } from "./stores/counter";
+import { storeToRefs } from "pinia";
+
+const store = useCounterStore();
+store.count++;
+
+// destructuring logic
+// for state we use storeToRefs, it'll create refs for every reactive property
+const { count, doubleCount } = storeToRefs(store);
+// for actions we can directly desturucture
+const { increment, decrement, waitAndAdd } = store;
+</script>
 <template>
   <main>
-    <h1>Vue Demo App</h1>
+    <div>
+      <h1>Using Store</h1>
+      <h2>Count is : {{ store.count }}</h2>
+      <h2>Double Count is : {{ store.doubleCount }}</h2>
+      <button @click="store.increment(1)">Increment</button>
+      <button @click="store.decrement">Decrement</button>
+      <button @click="store.waitAndAdd">Wait n Increment</button>
+    </div>
+
+    <!-- destructured -->
+    <div>
+      <h1>Using destructured store</h1>
+      <h2>Count is : {{ count }}</h2>
+      <h2>Double Count is : {{ doubleCount }}</h2>
+      <button @click="increment(1)">Increment</button>
+      <button @click="decrement">Decrement</button>
+      <button @click="waitAndAdd">Wait n Increment</button>
+    </div>
   </main>
 </template>
---
+++ src/stores/counter.js
@@ -0,0 +1,23 @@
+import { defineStore } from "pinia";
+
+export const useCounterStore = defineStore("counter", {
+  state: () => {
+    return { count: 0 };
+  },
+  actions: {
+    increment(val = 1) {
+      this.count += val;
+    },
+    decrement() {
+      this.count--;
+    },
+    async waitAndAdd() {
+      setTimeout(() => {
+        this.count++;
+      }, 2000);
+    },
+  },
+  getters: {
+    doubleCount: (state) => state.count * 2,
+  },
+});
