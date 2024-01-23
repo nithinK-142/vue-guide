09aa8d8a6afde2da4270bc9d2e90c9d21205519b
nithinK-142 <nithin.sagar.359@gmail.com>
2024-01-23 21:21:38 +0530

    feat: styles on active route.

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,19 @@
   <main>
     <h1>Vue Demo App</h1>
     <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-      <RouterLink to="/contact-us">Contact Us</RouterLink>
+      <RouterLink to="/" :class="{ 'active-link': $route.path === '/' }"
+        >Home</RouterLink
+      >
+      <RouterLink
+        to="/about"
+        :class="{ 'active-link': $route.path === '/about' }"
+        >About</RouterLink
+      >
+      <RouterLink
+        to="/contact-us"
+        :class="{ 'active-link': $route.path === '/contact-us' }"
+        >Contact Us</RouterLink
+      >
     </nav>
   </main>
   <RouterView />
@@ -15,4 +25,7 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <style scoped>
+.active-link {
+  background-color: hsla(160, 100%, 37%, 0.2);
+}
 </style>
