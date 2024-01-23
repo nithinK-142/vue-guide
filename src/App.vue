d739cead70fa719be6a23d663cfdfda688d12e35
nithinK-142 <nithin.sagar.359@gmail.com>
2024-01-23 21:05:33 +0530

    feat: routing.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
   <main>
     <h1>Vue Demo App</h1>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/contact-us">Contact Us</RouterLink>
+    </nav>
   </main>
+  <RouterView />
 </template>
+
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<style scoped>
+</style>
