ddd868f9536469c6eb05ab5c58d1bb5a5096a5e4
nithinK-142 <nithin.sagar.359@gmail.com>
2024-01-24 11:32:30 +0530

    feat: input focus.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
   <main>
     <h1>Vue Demo App</h1>
+    <input ref="myInput" />
+    <button @click="focusInput">Focus Input</button>
   </main>
 </template>
+
+<script setup>
+import { ref, nextTick } from "vue";
+
+const myInput = ref(null);
+
+const focusInput = () => {
+  nextTick(() => {
+    myInput.value.focus();
+  });
+};
+</script>
