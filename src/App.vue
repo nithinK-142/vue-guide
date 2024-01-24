55f8c361e044863f21833dd509a4fc644aadc1cb
nithinK-142 <nithin.sagar.359@gmail.com>
2024-01-24 18:17:23 +0530

    feat: input focus using custom directive.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
   <main>
     <h1>Vue Demo App</h1>
-    <input ref="myInput" />
-    <button @click="focusInput">Focus Input</button>
+    <input ref="myInput" v-inputfocus />
   </main>
 </template>
 
 <script setup>
-import { ref, nextTick } from "vue";
-
-const myInput = ref(null);
-
-const focusInput = () => {
-  nextTick(() => {
-    myInput.value.focus();
-  });
+const vInputfocus = {
+  mounted: (element) => {
+    element.focus();
+  },
 };
 </script>
