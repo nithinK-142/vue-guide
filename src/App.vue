d03442cfafba0b8e4c82a0b4c2e7a27f2f2dae5b
nithinK-142 <nithin.sagar.359@gmail.com>
2024-01-25 20:08:14 +0530

    feat: dark mode using vueuse and tailwind.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,55 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import { useDark, useToggle } from "@vueuse/core";
+import { ref } from "vue";
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+
+const navLinks = ref([
+  {
+    to: "/",
+    title: "Home",
+  },
+  {
+    to: "/about",
+    title: "About",
+  },
+  {
+    to: "/contact-us",
+    title: "Contact Us",
+  },
+]);
+</script>
+
 <template>
-  <main>
-    <h1>Vue Demo App</h1>
+  <main class="flex flex-col items-center relative">
+    <h1 class="text-xl">Vue Guide</h1>
+      <button
+        @click="toggleDark()"
+        class="px-3 py-1 rounded-full mb-4 text-black/60 dark:text-white text-3xl absolute right-8 top-[6px]"
+      >
+        {{ isDark ? "&#x2600;" : "&#x263E;" }}
+      </button>
     <nav>
-      <RouterLink to="/" :class="{ 'active-link': $route.path === '/' }"
-        >Home</RouterLink
-      >
-      <RouterLink
-        to="/about"
-        :class="{ 'active-link': $route.path === '/about' }"
-        >About</RouterLink
-      >
-      <RouterLink
-        to="/contact-us"
-        :class="{ 'active-link': $route.path === '/contact-us' }"
-        >Contact Us</RouterLink
-      >
+      <ul class="flex">
+        <li v-for="link in navLinks" :key="link.title">
+          <RouterLink
+            :to="`${link.to}`"
+            :class="{
+              'active-link': $route.path === link.to,
+            }"
+            class="px-2 py-1 mx-1 rounded-md"
+            >{{ link.title }}
+          </RouterLink>
+        </li>
+      </ul>
     </nav>
   </main>
   <RouterView />
 </template>
 
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-</script>
-
-<style scoped>
+<style>
 .active-link {
   background-color: hsla(160, 100%, 37%, 0.2);
 }
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <h1>Contact Us Page</h1>
+    <h1>Contact Us</h1>
   </div>
 </template>
